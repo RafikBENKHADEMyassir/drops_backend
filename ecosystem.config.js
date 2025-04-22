76c71e1557acf63c3fd0665475fe77ea5344d84f
Rafik BENKHADEM <44137439+RafikBENKHADEMyassir@users.noreply.github.com>
2025-04-22 17:28:25 +0100

    Refactor ecosystem.config.js for improved readability and maintainability

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,20 @@
+// Edit your ecosystem.config.js
 module.exports = {
-    apps: [{
-      name: "drops-backend",
-      script: "./dist/index.js",
-      instances: "max",
-      exec_mode: "cluster",
-      watch: false,
-      env: {
-        NODE_ENV: "production",
-        PORT: 3000
-      },
-      env_production: {
-        NODE_ENV: "production"
-      }
-    }]
-  }
+  apps: [{
+    name: "drops-backend",
+    script: "./dist/index.js",
+    instances: "max",
+    exec_mode: "cluster",
+    watch: false,
+    env: {
+      NODE_ENV: "production",
+      PORT: 3000
+    },
+    env_production: {
+      NODE_ENV: "production"
+    },
+    // Add this option to allow multiple instances to share the same port
+    listen_timeout: 50000,
+    kill_timeout: 5000
+  }]
+}
